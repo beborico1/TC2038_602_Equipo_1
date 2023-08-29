a52938b5694e5fdf1a48eabb1d2ca292ea0ac36b
DavidMDD <dm39988@gmail.com>
2023-08-29 10:00:56 -0600

    Añadi el deconstructor de la memoria alocada

--- kruskal/Kruskal_algorithm/Kruskal.cpp
+++ kruskal/Kruskal_algorithm/Kruskal.cpp
@@ -1,9 +1,9 @@
 /*
     Authors> 
         David Medina Domínguez (A01783155)
-        Luis
-        Atantza
-    Date> 28/08/2023
+        Luis Rico Almada (A01252831)
+        Arantza Parra Matrínez (A01782023) 
+    Date> 21/08/2023
     Abstract>
         In this work you will find the implementation of Kruskal’s Algorithm, which is a Minimum Spanning Tree (MST), so it makes a
         locally optimal choice in each step in order to find the optimal solution, in doing so, making it a greedy algorithm.
@@ -17,9 +17,8 @@
 #include <iostream>
 #include <windows.h> //Not necesary, only for development (adds Sleep() function)
 #include <fstream> //Library to read what is on a file (in/out)
-#include <sstream> 
+#include <sstream> //Stringstream library 
 #include <string>
-// #include <cctype> // Provides functions to classify and manipulate individual characters, (adds isspace() function)
 
 struct links{
     short weight;
@@ -29,10 +28,10 @@ struct links{
     struct links *previous;
 };
 
-struct links *indice = NULL; //Inicio de la lista
+struct links *indice = NULL; //First in list
 
 void add_sort(short weight_, short source_, short destination_){
-    links *link_; link_ = new links;//-----------------------------------------------capaz es links()
+    links *link_; link_ = new links;
     if (link_ == NULL){ // Check if the new "link" memory space did in fact was created
 		std::cout<<"Couldn't create new link"<<std::endl;
 		return;
@@ -46,7 +45,8 @@ void add_sort(short weight_, short source_, short destination_){
     struct links *indice_ = indice; //Create an auxiliary index (leading) to iterate through the double linked list
     struct links *_indice = NULL; //Create another auxiliary index (following)
 
-    if (indice == NULL){//If it is going to be the first link ot be added
+    //If it is going to be the first link to be added
+    if (indice == NULL){
         link_->next = NULL;
         link_->previous = NULL;
         indice =  link_;
@@ -55,20 +55,26 @@ void add_sort(short weight_, short source_, short destination_){
 
     while (indice_ != NULL){
         if(indice_->weight >= link_->weight){
+            //Set up right order between links
             link_->previous = _indice;
             link_->next = indice_;
             indice_->previous = link_;
-            if(_indice == NULL){//In case that it is has the lowest weight of the existing linked list
+            
+            //In case that it is has the lowest weight of the existing linked list
+            if(_indice == NULL){
                 indice = link_;
                 return;
             }
+            //In case that it is not the lowest weight on the list
             _indice->next = link_;
             return;
         }
-        _indice = indice_;//Iterate over
+        //Iterate over
+        _indice = indice_;
         indice_ = indice_->next;
     }
-    //If it is the link with the heaviest weight
+
+    //If it is the link with the heaviest weight (last)
     link_->next = NULL;
     link_->previous = _indice;
     _indice->next = link_;
@@ -76,9 +82,11 @@ void add_sort(short weight_, short source_, short destination_){
 }
 
 void print_links(){
-    struct links *indice_ = indice; //Create an auxiliary index to iterate through the double linked list
+    //Create an auxiliary index to print element on list
+    struct links *indice_ = indice; 
     short index; index = 1;
     
+
     while (indice_ != NULL){
         std::cout<< "[" << index <<"] Weight: " << indice_->weight << ", Source: " << indice_->source << ", Destination: " << indice_->destination<<std::endl;
         indice_ = indice_->next;
@@ -88,6 +96,17 @@ void print_links(){
 }
 
 
+void delete_links(links* index = indice){
+    if(index != NULL){
+        // std::cout<<index->weight<<std::endl;
+        delete_links(index->next);
+        delete index;
+    }
+}
+
+links* kruskal_algorithm(links* indece){
+    // links* kruskal_list[];
+}
 
 
 void readTXT(std::string test = "test1.txt"){
@@ -97,19 +116,17 @@ void readTXT(std::string test = "test1.txt"){
     if (file.is_open()){
         while (getline(file, line)){
             short weight_, source_, destination_;
-            // std::cout << line <<std::endl;
+
+            //Separate the string 'line' into the different values of weight, source and destination
             std::stringstream ssline(line);
             ssline >> weight_ >> source_ >> destination_;
-            // std::cout << weight_ <<std::endl;
-            // std::cout << source_ <<std::endl;
-            // std::cout << destination_ <<std::endl;
+            //Add to the existing list
             add_sort(weight_, source_, destination_);
-            // break;
-            // Sleep(1000);
         }
-
-    file.close();
+        //Close the file
+        file.close();
     }
+    //If the file was not found
     else std::cout << "Unable to open file"; 
 }
 
@@ -125,6 +142,6 @@ int main(int argc, char* argv[]){
     readTXT("test2.txt");
     print_links();
 
-
+    delete_links();
     return 0;
 }
