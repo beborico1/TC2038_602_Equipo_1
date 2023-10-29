5068019caf164b6ce4b225952a55add5d8952908
Arantza Parra <A01782023@tec.mx>
2023-10-29 17:42:01 -0600

    Find mCODE PATTERNS IN TRASMISIONS FILES

--- A01783155_ActInt1/actividad_integradora.cpp
+++ A01783155_ActInt1/actividad_integradora.cpp
@@ -28,26 +28,127 @@
 #include <sstream>
 #include <fstream> // Stringstream library
 #include <string>
+#include <vector>
 
-void readTXT(std::string test /*std::string test = "mcode1.txt"*/)
+std::string readTXT(std::string test /*std::string test = "mcode1.txt"*/)
 {
     std::string line;
     std::ifstream file(test);
+    std::ostringstream wholeFile; 
 
     if (file.is_open())
     {
         while (getline(file, line))
         {
-            std::cout << line << std::endl;
+            wholeFile << line << '\n';
         }
         file.close(); // Close the file
     }
     else std::cout << "Unable to open file";
+    return wholeFile.str();
 }
+// Parte #1: Códigos maliciosos
+// Función para construir la tabla 'pi' para el algoritmo KMP.
+std::vector <int> getPi (std::string pattern) {
+    int pattern_length = pattern.length();
+    std::vector<int> pi(pattern_length, 0); // Inicializar el vector 'pis' con ceros.
+    int prefix_length = 0; // longitud del prefijo mas largo
+    int pattern_index = 1; // índice para recorrer el patrón
+
+    // construir la tabla 'pi'
+    while (pattern_index < pattern_length) {
+        // si los caracteres coinciden, se incrementa 1 la longitud del prefijo.
+        if (pattern[prefix_length] == pattern[pattern_index]) {
+            prefix_length++;
+            pi[pattern_index] = prefix_length;
+            pattern_index++;
+        } else { // si no coinciden los caracteres
+            // si la longitud del prefijo es cero se incrementa el índice del patrón.
+            if (prefix_length == 0) {
+                pi[pattern_index] = prefix_length;
+                pattern_index++;
+            } else {
+                // si no hay coincidencia, retrocede en la tabla 'pi' 
+                prefix_length = pi[prefix_length - 1];
+            }
+        }
+    }
+    return pi;
+}
+
+// algoritmo KMP para buscar un patrón en un texto
+int  KMPSearch(const std::string text, const std::string pattern) {
+    int text_length = text.length(); // longitud del texto
+    int pattern_length = pattern.length(); // longitud del patrón
+    // obtener la tabla 'pi' para el patrón
+    std::vector<int> pi = getPi(pattern); 
+
+    // std::cout << "Tabla pi del patrón: " << std::endl;
+    // for (int pi_index = 0; pi_index < pi.size(); pi_index++) {
+    //     std::cout << pi[pi_index] << " ";
+    // }
+    // std::cout << std::endl;
+
+    int text_index = 0; // índice para el texto
+    int pattern_index = 0; // índice para el patrón
+
+    while (text_index < text_length) {
+        // si hay una coincidencia, incrementa ambos índices
+        if (pattern[pattern_index] == text[text_index]) {
+            text_index++;
+            pattern_index++;
+        }
+
+        // si el índice del patrón es igual a la longitud del patrón, se encontró el patrón
+        if (pattern_index == pattern_length) {
+            return text_index - pattern_index; 
+        } else if (text_index < text_length && pattern[pattern_index] != text[text_index]) {
+            // si no hay coincidencia y el índice del texto es menor que la longitud del texto
+            if (pattern_index != 0) { // si el índice del patrón no es cero
+                pattern_index = pi[pattern_index - 1]; // retrocede en la tabla 'pi'
+            } else { // si el índice del patrón es cero
+                text_index++; // incrementa el índice del texto
+            }
+        }
+    }
+    return -1;
+}
+// Busca secuencia de mcode en fragmento de transmission y regresa la posición donde se encuentra
+void FindSecuence(const std::string transmission, const std::string mcode1, const std::string mcode2, const std::string mcode3) {
+    int position;
+
+    position = KMPSearch(transmission, mcode1);
+    if (position != -1) {
+        std::cout << "true " << position + 1 << std::endl;
+    } else {
+        std::cout << "false" << std::endl;
+    }
+
+    position = KMPSearch(transmission, mcode2);
+    if (position != -1) {
+        std::cout << "true " << position + 1 << std::endl;
+    } else {
+        std::cout << "false" << std::endl;
+    }
+
+    position = KMPSearch(transmission, mcode3);
+    if (position != -1) {
+        std::cout << "true " << position + 1 << std::endl;
+    } else {
+        std::cout << "false" << std::endl;
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello world" << std::endl;
-    readTXT("mcode1.txt");
-    return 0;
+    // std::cout << "Hello world" << std::endl;
+    // readTXT("mcode1.txt");
+    std::string transmission = readTXT("transmission1.txt");
+     std::string transmission2 = readTXT("transmission2.txt");
+    std::string mcode1 = readTXT("mcode1.txt");
+    std::string mcode2 = readTXT("mcode2.txt");
+    std::string mcode3 = readTXT("mcode3.txt");
+    FindSecuence(transmission, mcode1, mcode2, mcode3);
+    FindSecuence(transmission2, mcode1, mcode2, mcode3);
 }
