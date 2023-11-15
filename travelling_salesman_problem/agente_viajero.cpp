75d9cc70c6806012b54db4372d95a3fdd21d71e2
Dav <dm39988@gmail.com>
2023-11-14 19:20:09 -0600

    Last changes

--- travelling_salesman_problem/agente_viajero.cpp
+++ travelling_salesman_problem/agente_viajero.cpp
@@ -15,6 +15,7 @@
 
 #include <iostream>
 #include <array>
+#include <vector>
 #include <limits> //Solo para encontrar el numero mas grande de un tipo de dato
 
 int test_arr[5][5] = {
@@ -39,24 +40,50 @@ int get_sum_of_indexes(const int (&arr)[rows][columns], const int (&arr_indexes)
 }
 
 template <int columns, int rows> // Estos son un template que seran remplazados por el correcto type cuando sean instanceadas :o
-int get_relaxed_weight(const int (&arr)[rows][columns]){ //Esto es basicamente un BFS
+int get_relaxed_weight(const int (&arr)[rows][columns]){
     std::array <int, columns> chosen_indexes;
-    int relaxed_weight = std::numeric_limits<int>::max();
+    int max_weight = std::numeric_limits<int>::max();
+    int relaxed_weight = 0;
 
-    //Aqui hacer una función recursiva que haga un BFS y que regrese el peso relajado
-    auto BFS = [](int a, int b) -> int {
-        if()
-        return BFS();
-    };
+    for(int i = 0; i < columns; ++i){
+        int relaxed_col = max_weight;
+        for(int j = 0; j < rows; ++j){
+            if(arr[i][j] <= relaxed_col && arr[i][j] != 0)
+                relaxed_col = arr[i][j]; 
+        }
+        relaxed_weight += relaxed_col; 
+    }
     
     return relaxed_weight;
 }
 
+bool in_array(int value, const std::vector<int>& arr){
+    for(int i = 0; i < arr.size(); ++i){
+        if(arr[i] == value)
+            return true;
+    }
+    return false;
+}
+
+// std::array <int, rows> get_hamiltonian_cycle(const int (&arr)[rows][columns]){
 template <int columns, int rows>
-std::array <int, rows> get_hamiltonian_cycle(const int (&arr)[rows][columns], int relaxed_weight){
-    std::array<int, rows> result;
-    int relaxed_weight = get_relaxed_weight(arr);   
-    //Aqui si se tiene que hacer el hamiltonian cycle (la que esta en "Travelling Salesman Problem.pdf")
+int get_hamiltonian_cycle(const int (&arr)[rows][columns]){
+    int max_weight = std::numeric_limits<int>::max();
+    int result = 0;
+    std::vector<int> chosenColumns;
+
+    for(int i = 0; i < rows; ++i){  
+        int weight = max_weight;
+        int col = 0;
+        for(int j = 0; j < columns; ++j){
+            if(arr[i][j] <= weight  && arr[i][j] != 0 && !in_array(j, chosenColumns)){
+                col = j;
+                weight = arr[i][j];
+            }
+        }
+        chosenColumns.push_back(col);
+        result += weight;
+    }
 
     return result;
 }
@@ -73,13 +100,16 @@ void print_2D_array(const int (&arr)[rows][columns]){
 
 int main() {
     int A[5][5];
-    std::cout << std::max(1, 2) << std::endl; 
 
-    get_relaxed_weight(test_arr);
-
-    std::cout << get_sum_of_indexes(test_arr, test_arr_indexes) << std::endl;
-
+    std::cout<< "Matrix: " <<std::endl;
     print_2D_array(test_arr);
+
+    std::cout<< "Relaxed Weight: " <<std::endl;
+    std::cout <<  get_relaxed_weight(test_arr) << std::endl; 
+
+    std::cout<< "Hamiltonian Cycle: " <<std::endl;
+    std::cout <<  get_hamiltonian_cycle(test_arr) << std::endl; 
+
 
     return 0;
 }
